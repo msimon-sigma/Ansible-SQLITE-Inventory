48e00706f5c0775e864334e6a6838413c93b4b57
msimon-sigma <32294325+msimon-sigma@users.noreply.github.com>
2021-05-03 14:32:15 +0200

    Update inventory.py
    FIX: Add missing host with no group in group table

--- inventory.py
+++ inventory.py
@@ -6,6 +6,9 @@ import sqlite3
 # Format Ansible expects for all
 # { "group1": { "hosts": [], "vars": {} }, "group2": { "hosts": [], "vars": {} }, "_meta": {"hostvars": {}} }
 
+def init_sample_if_empty(): 
+    return
+
 try:
     import json
 except ImportError:
@@ -23,33 +26,53 @@ def query_db(query):
 
 def query_all_hosts():
     return [row[0] for row in query_db('SELECT hostname from hosts')]
+def query_group_list():
+    return [row[0] for row in query_db('SELECT groupname from groups')]
+def query_group_host():
+    return [row[0] for row in query_db('SELECT groupname from hosts')]
 def query_members_of(groupname):
     return [ ix[0] for ix in query_db('SELECT hostname from hosts WHERE groupname = "'+groupname+ '"') ]
 def query_children_of(groupname):
     return [ ix[0] for ix in query_db('SELECT groupname from groups WHERE children_of = "'+groupname+ '"') ]
-def query_group_list():
-    return [row[0] for row in query_db('SELECT groupname from groups')]
+
+
+    
 def query_group_vars(groupname):
     result = [dict(ix) for ix in query_db('SELECT * from groups WHERE groupname = "'+groupname+ '"') ]
-    return result[0]
+    try:
+        return result[0]
+    except:
+        return {}
 def query_host_args(hostname):
+    vars_dict = {}
     query_result=query_db('SELECT * FROM hosts WHERE hostname = "' + hostname + '"')
     result = [dict(ix) for ix in query_result]
-    return result[0]
+    try:
+        return result[0]
+    except:
+        return {}
 
 def main():
+    init_sample_if_empty()
     inventory_dict = dict()
     inventory_dict['_meta'] = {}
     inventory_dict['_meta']['hostvars'] = {}
     unprocessed_hosts = query_all_hosts()
-    for groupname in list(filter(None,query_group_list())):
+    # group in groups table and in host tables
+    full_grouplist = list(dict.fromkeys(query_group_list() + query_group_host()))
+    # build group bloc
+    for groupname in full_grouplist:
+        if groupname == '':
+            continue
         inventory_dict[groupname] = {}
         inventory_dict[groupname]['hosts'] = query_members_of(groupname)
         inventory_dict[groupname]['vars'] = query_group_vars(groupname)
         inventory_dict[groupname]['children'] = query_children_of(groupname)
+        # Append hostvars to _meta
         for hostname in query_members_of(groupname):
             inventory_dict['_meta']['hostvars'][hostname] = query_host_args(hostname)
             unprocessed_hosts.remove(hostname)
+    # ungrouped
     for hostname in unprocessed_hosts :
         inventory_dict['_meta']['hostvars'][hostname] = query_host_args(hostname)
     inventory_dict['all'] = {}
